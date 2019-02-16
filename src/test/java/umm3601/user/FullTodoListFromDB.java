273c9845c68ab25a81be7305c4b30c95cd36fad3
deglm006 <deglm006@morris.umn.edu>
2019-02-16 17:05:43 -0600

    Added test to ensure the right number of todos with a true status are returned for "api/todos"

--- src/test/java/umm3601/user/FullTodoListFromDB.java
+++ src/test/java/umm3601/user/FullTodoListFromDB.java
@@ -8,9 +8,16 @@ import static org.junit.Assert.assertEquals;
 
 public class FullTodoListFromDB {
   @Test
-  public void totalUserCount() throws IOException {
+  public void totalTodoCount() throws IOException {
     TodoDatabase db = new TodoDatabase("src/main/data/todos.json");
     Todo[] allTodos = db.listTodos();
     assertEquals("Incorrect total number of todos", 300, allTodos.length);
+  }
+
+  @Test
+  public void totalTodoTrueCount() throws IOException {
+    TodoDatabase db = new TodoDatabase("src/main/data/todos.json");
+    Todo[] trueTodos = db.filterStatusTodo(true);
+    assertEquals("Incorrect number of True todos", 143, trueTodos.length);
   }
 }
